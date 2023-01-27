5f34e721e974e6edda43c84de208c972c4a8029b
Alexandre Gravem <gravem@gmail.com>
2023-01-27 10:39:10 -0300

    crud and validation

--- src/clj/guestbook/routes/home.clj
+++ src/clj/guestbook/routes/home.clj
@@ -2,13 +2,35 @@
   (:require
    [guestbook.layout :as layout]
    [guestbook.db.core :as db]
-   [clojure.java.io :as io]
    [guestbook.middleware :as middleware]
    [ring.util.response]
-   [ring.util.http-response :as response]))
+   [ring.util.http-response :as response]
+   [struct.core :as st]))
 
-(defn home-page [request]
-  (layout/render request "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
+(defn max-length [len]
+  {:message (str "must be at least " len " characters long")
+   :validate (fn [msg] (>= (count msg) len))})
+
+(def message-schema
+  [[:name st/required st/string]
+   [:message st/required st/string (max-length 5)]])
+
+(defn validate-params [params]
+  (first (st/validate params message-schema)))
+
+(defn home-page [{:keys [flash] :as request}]
+  (layout/render request
+                 "home.html"
+                 (merge {:messages (db/get-messages)}
+                        (select-keys flash [:name :message :errors]))))
+
+(defn save-message! [{:keys [params]}]
+  (if-let [errors (validate-params params)]
+    (-> (response/found "/")
+        (assoc :flash (assoc params :errors errors)))
+    (do
+      (db/save-message! params)
+      (response/found "/"))))
 
 (defn about-page [request]
   (layout/render request "about.html"))
@@ -18,5 +40,6 @@
    {:middleware [middleware/wrap-csrf
                  middleware/wrap-formats]}
    ["/" {:get home-page}]
+   ["/message" {:post save-message!}]
    ["/about" {:get about-page}]])
 
